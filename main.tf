6079e7451e32447cad8f98884723f7f91a41d62e
philiwon8868 <wongp2009@gmail.com>
2021-06-27 00:47:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,18 +37,18 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  for_each = var.epgs
-  name             = "${each.value.epg}"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  network_interface {
-    network_id = "${data.vsphere_network.network[each.value.epg].id}"
-  }
-  depends_on = [
-  aci_epg_to_domain.terraform_epg_domain,
-  ]	
-}
+#resource "vsphere_virtual_machine" "vm" {
+#  for_each = var.epgs
+#  name             = "${each.value.epg}"
+#  resource_pool_id = data.vsphere_resource_pool.pool.id
+#  datastore_id     = data.vsphere_datastore.datastore.id
+#  network_interface {
+#    network_id = "${data.vsphere_network.network[each.value.epg].id}"
+#  }
+#  depends_on = [
+#  aci_epg_to_domain.terraform_epg_domain,
+#  ]	
+#}
 
 # Configure the provider with your Cisco APIC credentials.
 provider "aci" {
