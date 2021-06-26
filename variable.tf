12d299301ede4096c4a7d9208a769dba70819ec3
philiwon8868 <wongp2009@gmail.com>
2021-06-26 21:21:13 +0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -165,7 +165,7 @@ variable "Devices" {
            outside_vlan = "1089"
            phy_domain = "phys"
            phy_vlan_pool = "Phys-Pool"
-           contract = "Con_Web_to_App"
+           contract = "SG_ASA1000v"
        }
     }
 }
