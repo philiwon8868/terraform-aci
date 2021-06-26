7e4639ddc0b0c1375278b8894ad440b30c346106
philiwon8868 <wongp2009@gmail.com>
2021-06-26 23:18:50 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -342,9 +342,10 @@ resource "aci_logical_interface_context" "consumer" {
 	permit_log                       = "no"
   relation_vns_rs_l_if_ctx_to_l_if = "${aci_tenant.terraform_tenant.id}/lDevVip-${each.value.name}/lIf-${each.value.outside_interface}"
   relation_vns_rs_l_if_ctx_to_bd   = "${aci_tenant.terraform_tenant.id}/BD-${each.value.outside_bd}"
-#  relation_vns_rs_l_if_ctx_to_svc_redirect_pol = aci_service_redirect_policy.pbr[each.value.pbr_name].id
+  relation_vns_rs_l_if_ctx_to_svc_redirect_pol = aci_service_redirect_policy.pbr[each.value.name].id
   depends_on = [
     aci_rest.device,
+    aci_service_redirect_policy.pbr,
   ]
 }
 
@@ -356,9 +357,10 @@ resource "aci_logical_interface_context" "provider" {
 	permit_log                       = "no"
   relation_vns_rs_l_if_ctx_to_l_if = "${aci_tenant.terraform_tenant.id}/lDevVip-${each.value.name}/lIf-${each.value.inside_interface}"
   relation_vns_rs_l_if_ctx_to_bd   = "${aci_tenant.terraform_tenant.id}/BD-${each.value.inside_bd}"
- # relation_vns_rs_l_if_ctx_to_svc_redirect_pol = aci_service_redirect_policy.pbr[each.value.pbr_name].id
+  relation_vns_rs_l_if_ctx_to_svc_redirect_pol = aci_service_redirect_policy.pbr[each.value.name].id
   depends_on = [
     aci_rest.device,
+    aci_service_redirect_policy.pbr,
   ]
 }
 
