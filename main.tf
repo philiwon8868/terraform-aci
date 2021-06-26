ef4ae3632a38de032af2db9be93c011fb12f3fa3
philiwon8868 <wongp2009@gmail.com>
2021-06-26 10:43:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -343,7 +343,7 @@ resource "aci_logical_interface_context" "provider" {
 	l3_dest                          = "yes"
 	permit_log                       = "no"
   relation_vns_rs_l_if_ctx_to_l_if = "${aci_tenant.terraform_tenant.id}/lDevVip-${each.value.name}/lIf-${each.value.inside_interface}"
-  relation_vns_rs_l_if_ctx_to_bd   = each.value.inside_bd
+  relation_vns_rs_l_if_ctx_to_bd   = "${aci_tenant.terraform_tenant.id}/BD-${each.value.inside_bd}"
  # relation_vns_rs_l_if_ctx_to_svc_redirect_pol = aci_service_redirect_policy.pbr[each.value.pbr_name].id
   depends_on = [
     aci_rest.device,
